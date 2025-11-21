7504d2b4f7fae42cc3afc31878dd8360b2deab32
dian <jindian_0805@foxmail.com>
2025-11-21 15:59:32 +0800

    Initial Commit

---
+++ LatexVectorTool/LatexVectorToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  LatexVectorToolApp.swift
+//  LatexVectorTool
+//
+//  Created by Dian Jin on 21/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct LatexVectorToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
